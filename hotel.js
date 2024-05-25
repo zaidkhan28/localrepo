ed886c7cb40012e20caaafdd3c4f26f0609b6153
zaid khan <nk9785057@gmail.com>
2024-05-25 11:21:37 +0530

    new hotel

---
+++ hotel.js
@@ -0,0 +1,17 @@
+function one(){
+    document.getElementById("sliderimage").src="img1.jpg";
+}
+function two(){
+    document.getElementById("sliderimage").src="img2.jpg";
+}
+function three(){
+    document.getElementById("sliderimage").src="img3.jpg";
+}
+function four(){
+    document.getElementById("sliderimage").src="img4.jpg";
+}
+
+setInterval(one,8000);
+setInterval(two,12000);
+setInterval(three,16000);
+setInterval(four,20000);
